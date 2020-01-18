c647973dc2e5396bc6515372446ab0e860831cd9
Union Hills Software <unionhills@cox.net>
2020-01-18 15:08:04 -0700

    Modified to look for PORT env variable, otherwise default to 4200

--- server/main.ts
+++ server/main.ts
@@ -2,8 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
 
 async function bootstrap() {
+  const port = parseInt(process.env.PORT, 10) || 4200;
   const app = await NestFactory.create(ApplicationModule);
+
   app.setGlobalPrefix('api');
-  await app.listen(4200);
+  await app.listen(port);
+
+  console.log("App listening on port " + port);
 }
+
 bootstrap();
